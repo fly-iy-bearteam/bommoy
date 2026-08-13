484cbd6d123c76af27fccebc861a8136e2268a5f
fly-iy-bearteam <miucleoai@gmail.com>
2026-08-13 10:39:05 +0700

    Delete JS directory

--- JS/supabase.js
+++
@@ -1,5 +0,0 @@
-// Khởi tạo Supabase Client
-const SUPABASE_URL = 'https://fpiyoysdxosvgrjdufan.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZwaXlveXNkeG9zdmdyamR1ZmFuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODY1ODI0MzUsImV4cCI6MjEwMjE1ODQzNX0.Mj0L-BMaXEEVzuLDX_flY-aUuy1US9GD7UBhIXWgTAI';
-
-const supabase = supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
